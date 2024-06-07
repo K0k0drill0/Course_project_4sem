d6ea2089fa4c1e060ce52f029a1afb9d4c90037b
K0k0drill0 <asanastasia0201@gmail.com>
2024-06-08 02:02:09 +0300

    done

---
+++ build/CMakeFiles/course_project.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_boundary_tags.cpp" "CMakeFiles/course_project.dir/allocators/allocator_boundary_tags.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_boundary_tags.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_buddies_system.cpp" "CMakeFiles/course_project.dir/allocators/allocator_buddies_system.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_buddies_system.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_global_heap.cpp" "CMakeFiles/course_project.dir/allocators/allocator_global_heap.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_global_heap.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_guardant.cpp" "CMakeFiles/course_project.dir/allocators/allocator_guardant.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_guardant.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_red_black_tree.cpp" "CMakeFiles/course_project.dir/allocators/allocator_red_black_tree.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_red_black_tree.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/allocators/allocator_sorted_list.cpp" "CMakeFiles/course_project.dir/allocators/allocator_sorted_list.cpp.o" "gcc" "CMakeFiles/course_project.dir/allocators/allocator_sorted_list.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/logger/client_logger.cpp" "CMakeFiles/course_project.dir/logger/client_logger.cpp.o" "gcc" "CMakeFiles/course_project.dir/logger/client_logger.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/logger/client_logger_builder.cpp" "CMakeFiles/course_project.dir/logger/client_logger_builder.cpp.o" "gcc" "CMakeFiles/course_project.dir/logger/client_logger_builder.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/logger/logger.cpp" "CMakeFiles/course_project.dir/logger/logger.cpp.o" "gcc" "CMakeFiles/course_project.dir/logger/logger.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/logger/logger_builder.cpp" "CMakeFiles/course_project.dir/logger/logger_builder.cpp.o" "gcc" "CMakeFiles/course_project.dir/logger/logger_builder.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/logger/logger_guardant.cpp" "CMakeFiles/course_project.dir/logger/logger_guardant.cpp.o" "gcc" "CMakeFiles/course_project.dir/logger/logger_guardant.cpp.o.d"
+  "/home/kokodrillo/git_projects/Course_Project_4sem/main.cpp" "CMakeFiles/course_project.dir/main.cpp.o" "gcc" "CMakeFiles/course_project.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/course_project.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/course_project.dir/allocators/allocator_boundary_tags.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_boundary_tags.cpp.o.d"
+  "CMakeFiles/course_project.dir/allocators/allocator_buddies_system.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_buddies_system.cpp.o.d"
+  "CMakeFiles/course_project.dir/allocators/allocator_global_heap.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_global_heap.cpp.o.d"
+  "CMakeFiles/course_project.dir/allocators/allocator_guardant.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_guardant.cpp.o.d"
+  "CMakeFiles/course_project.dir/allocators/allocator_red_black_tree.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_red_black_tree.cpp.o.d"
+  "CMakeFiles/course_project.dir/allocators/allocator_sorted_list.cpp.o"
+  "CMakeFiles/course_project.dir/allocators/allocator_sorted_list.cpp.o.d"
+  "CMakeFiles/course_project.dir/logger/client_logger.cpp.o"
+  "CMakeFiles/course_project.dir/logger/client_logger.cpp.o.d"
+  "CMakeFiles/course_project.dir/logger/client_logger_builder.cpp.o"
+  "CMakeFiles/course_project.dir/logger/client_logger_builder.cpp.o.d"
+  "CMakeFiles/course_project.dir/logger/logger.cpp.o"
+  "CMakeFiles/course_project.dir/logger/logger.cpp.o.d"
+  "CMakeFiles/course_project.dir/logger/logger_builder.cpp.o"
+  "CMakeFiles/course_project.dir/logger/logger_builder.cpp.o.d"
+  "CMakeFiles/course_project.dir/logger/logger_guardant.cpp.o"
+  "CMakeFiles/course_project.dir/logger/logger_guardant.cpp.o.d"
+  "CMakeFiles/course_project.dir/main.cpp.o"
+  "CMakeFiles/course_project.dir/main.cpp.o.d"
+  "course_project"
+  "course_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/course_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
